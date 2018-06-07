a9337a4285e2d9cd619057c73f9a0de927008ea1
Elif Aslan <elas@de.ibm.com>
2018-06-07 15:45:06 +0200

    adding s390x_tests
    Add new testcase TOOL_s390_CHCHP
    
    Add new testcase TOOL_s390_CHCHP
    
    Add new testcase TOOL_s390_hyptop
    
    Change TOOL_s390_vmcp scripts
    
    Change TOOL_s390_vmcp README file
    
    Change common.sh
    
    Change lib/common.sh
    
    modified TOOL_s390_hyptop/hyptop.sh TOOL_s390_vmcp/vmcp_main.sh
    
    modified TOOL_s390_hyptop/hyptop.sh TOOL_s390_vmcp/vmcp_main.sh
    
    delete TOOL_s390_CHCHP
    
    add changed scripts in TOOL_s390_CHCHP and lib/
    
    remove TOOL_s390_CHCHP/
    
    add TOOL_s390_chchp//
    
    remove consoletest_hyptop.pm
    
    remove consoletest_vmcp.pm
    
    remove files
    
    add README.md
    
    add README.md
    
    added main_common.pm s390base.pm
    
    Create README.md
    
    added new Test case TOOL_s390_qethqoat
    
    added new Test case TOOL_s390_qetharp
    
    added new Test case TOOL_s390_kernel_1
    
    delete some script
    
    delete some script
    
    added new Test case TOOL_s390_kernel_1
    
    added new Test case TOOL_s390_fcp
    
    added new Test case KERNEL_btrfs
    
    added new Test case MEMORY_chmem
    
    added new Test case MEMORY_memtester
    
    added new Test case DASD_LVM_Regression
    
    fixed hyptop.sh
    
    Move main_common.pm s390base.pm to os-autoinst-distri-opensuse/lib
    
    Remove main_common.pm s390base.pm from 'tests/s390x_tests/lib
    
    Move DASD_LVM_Regression to /os-autoinst-distri-opensuse/data
    
    Remove DASD_LVM_Regression from /tests/s390x_tests
    
    Move DASD_LVM Regression KERNEL_btrfs to /os-autoinst-distri-opensuse/data/s390x
    
    Move all s390x tests to /os-autoinst-distri-opensuse/data/s390x
    
    Move lib to /os-autoinst-distri-opensuse/data/s390x
    
    Add new test case TOOL_s390_chccwdev /os-autoinst-distri-opensuse/data/s390x
    
    Remove common.sh entry from all files
    
    Remove duplicate consoletest for qetharp qethqoat
    
    Add new test case TOOL_s390_ZIOMON
    
    Add new test case TOOL_s390_vmconvert
    
    Add new test case TOOL_s390_chzdev_lszdev
    
    Add new test case KERNEL_LSCPU_CHCPU

--- lib/main_common.pm
+++ lib/main_common.pm
@@ -1064,6 +1064,21 @@ sub load_consoletests {
     loadtest "console/prepare_test_data";
     loadtest "console/consoletest_setup";
     loadtest 'console/integration_services' if is_hyperv || is_vmware;
+
+    if (get_var('IBM_TESTS')) {
+        # prepare tarballs for the testcase
+        # the path below should be reworked to be universal for any distribution, now it's for openQA deployed on opensuse
+        my $tcs_path = "/var/lib/openqa/share/tests/sle/data/s390x/";
+        system("cd ${tcs_path}lib && rm -f common.tgz && tar -czf common.tgz ./*.sh");
+
+        my $testset = get_var('IBM_TESTSET'); # e.g. "KERNEL or TOOL or MEMORY"
+        foreach my $tc (split(',', get_var('IBM_TESTS'))) {
+               system("cd ${tcs_path}${testset}${tc} && rm -f ${testset}${tc}.tgz && tar -czf ${testset}${tc}.tgz ./*");
+               loadtest "s390x_tests/consoletest_${testset}${tc}";
+            }
+            return 1;
+      }
+
     loadtest "locale/keymap_or_locale";
     loadtest "console/orphaned_packages_check" if is_jeos;
     loadtest "console/force_scheduled_tasks" unless is_jeos;
